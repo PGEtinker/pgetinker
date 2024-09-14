77d30d8c2e12b852a56e1734eb464d0c9d38905a
Moros Smith <moros1138@gmail.com>
2024-09-14 05:39:30 -0400

    add library version configuration to the storage conforming function

--- resources/js/lib/storage.ts
+++ resources/js/lib/storage.ts
@@ -40,6 +40,31 @@ export function conformStorage(): void
     {
         setStorageValue("editor.inlayHints.enabled", false);
     }
+    
+    if(getStorageValue("olcPixelGameEngine") == null)
+    {
+        setStorageValue("olcPixelGameEngine", "v2.27");
+    }
+
+    if(getStorageValue("miniaudio") == null)
+    {
+        setStorageValue("miniaudio", "0.11.21");
+    }
+    
+    if(getStorageValue("olcPGEX_Gamepad") == null)
+    {
+        setStorageValue("olcPGEX_Gamepad", "f5958a5");
+    }
+    
+    if(getStorageValue("olcPGEX_MiniAudio") == null)
+    {
+        setStorageValue("olcPGEX_MiniAudio", "v1.7");
+    }
+
+    if(getStorageValue("olcSoundWaveEngine") == null)
+    {
+        setStorageValue("olcSoundWaveEngine", "v0.02");
+    }
 }
 
 export function getStorageValue(key: string): string | null
