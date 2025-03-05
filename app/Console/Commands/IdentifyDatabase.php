b7a8906199ecddb1a75d8780e7ef97ca274d59aa
Moros Smith <moros1138@gmail.com>
2025-03-04 20:36:18 -0500

    rename duplicate class name

--- app/Console/Commands/IdentifyDatabase.php
+++ app/Console/Commands/IdentifyDatabase.php
@@ -6,7 +6,7 @@ use App\Models\Code;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
-class MoveDatabase extends Command
+class IdentifyDatabase extends Command
 {
     /**
      * The name and signature of the console command.
