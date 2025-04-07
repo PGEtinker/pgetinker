42b89633c63254ea5a44cb993f98be9f6c9e4dda
Moros Smith <moros1138@gmail.com>
2025-04-07 12:13:09 -0400

    main: update app:move-database - move codes from supabase to the configured default database.

--- app/Console/Commands/MoveDatabase.php
+++ app/Console/Commands/MoveDatabase.php
@@ -20,25 +20,36 @@ class MoveDatabase extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Move data from supabase to the default database.';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        echo "might wanna revamp this first\n\n";
-        return;
-        
         //
-        $codes = DB::connection('development')->select("SELECT * FROM codes;");
+        $codes = DB::connection('supa')
+            ->table('codes')
+            ->orderBy('id', 'asc')
+            ->get();
+        
         for($i = 0; $i < count($codes); $i++)
         {
+            echo $codes[$i]->slug . "\n";
             $code = new Code();
-            $code->slug = $codes[$i]->slug;
-            $code->code = $codes[$i]->code;
-            $code->hash = $codes[$i]->hash;
+            $code->id         = $codes[$i]->id;
+            $code->slug       = $codes[$i]->slug;
+            $code->code       = $codes[$i]->code;
+            $code->hash       = $codes[$i]->hash;
+            $code->view_count = $codes[$i]->view_count;
+            $code->created_at = $codes[$i]->created_at;
             $code->save();
         }
+        
+        // Set the new auto-increment value (e.g., after the highest ID)
+        $maxId = Code::max('id');
+        $newAutoIncrement = $maxId + 1;
+        echo "New Auto Increment: " . $newAutoIncrement . "\n\n";
+        DB::query("ALTER TABLE codes AUTO_INCREMENT = " . $newAutoIncrement);
     }
 }
