48c38077f49f1b02fc7809a2db793316cbc4836d
Moros Smith <moros1138@gmail.com>
2025-04-09 13:39:56 -0400

    main: add library versions demo

--- resources/example3.cpp
+++ resources/example3.cpp
@@ -1,10 +1,19 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
+#define OLC_PGEX_MINIAUDIO
+#include "olcPGEX_MiniAudio.h"
+
 #if defined(__EMSCRIPTEN__)
 #include <emscripten.h>
 #endif
 
+// EMSCRIPTEN ONLY!
+// 
+// At runtime, this function attempts to load a file
+// from the provided URL, and maps it to emscripten's
+// filesystem. You can then use the file in any C/C++
+// filesystem function as if it were on the local disk.
 void FILE_RESOLVE(const char* url, const char* file)
 {
     #if defined(__EMSCRIPTEN__)
@@ -20,62 +29,112 @@ public:
     Example()
     {
         // Name your application
-        sAppName = "Example 3";
+        sAppName = "Library Versions Demo";
     }
     
 public:
+    
+    // OnUserCreate is Called once at the start and
+    // is where you do things like load files and
+    // initilize variables.
     bool OnUserCreate() override
     {
-        // Called once at the start, so create things here
-        
-        // built with emscripten, maps the url to the virtual filesystem and makes it
-        // available to the standard C/C++ file i/o functions without change!
-        //
-        // built with any other native toolchain, the macro does nothing and all file
-        // access is done just as it would in any other normal scenario.
+        // load "assets/gfx/broken.png" from a URL
         FILE_RESOLVE("https://pit.pgetinker.com/MwpptUlwPhnc.png", "assets/gfx/broken.png");
         
-        renBroken.Load("assets/gfx/broken.png");
-
         color = RandomColor();
+        
+        // PGE version
+        versions = "olcPixelGameEngine: v" + std::to_string(PGE_VER / 100) + "." + std::to_string(PGE_VER % 100) + "\n";
+        
+        // olcPGEX_MiniAudio version
+        size_t spacePos = ma.name.find(' ');
+        if(spacePos != std::string::npos)
+        {
+            std::string name = ma.name.substr(0, spacePos);
+            std::string version = ma.name.substr(spacePos + 1);
+            versions += name + ":  " + version + "\n";
+        }
+        
+        // MiniAudio version
+        versions += "MiniAudio:          ";
+        versions += MA_VERSION_STRING; versions += "\n";
+        
         return true;
     }
     
+    // OnUserUpdate is called once per frame and
+    // is where you draw things to the screen
     bool OnUserUpdate(float fElapsedTime) override
     {
-        // Called once per frame, draws random coloured pixels
+        // when you left click the mouse
         if(GetMouse(0).bPressed)
+        {
+            // change the color
             color = RandomColor();
-        
+            
+            // print out the current mouse position (x, y)
+            std::cout << GetMousePos() << "\n";
+        }
+            
+        // clear the screen to the provided color
         Clear(color);
-        DrawRect(0,0,ScreenWidth()-1, ScreenHeight()-1, olc::YELLOW);
-        DrawString(6,  6, "Hello, PGE", olc::BLACK);
-        DrawString(5,  5, "Hello, PGE", olc::WHITE);
-        DrawString(6, 26, "Mouse position SHOULD match\nclosely to the circle.\n\nYellow borders should ALWAYS\nbe visible\n\nLEFT MOUSE to change color.", olc::BLACK);
-        DrawString(5, 25, "Mouse position SHOULD match\nclosely to the circle.\n\nYellow borders should ALWAYS\nbe visible\n\nLEFT MOUSE to change color.", olc::WHITE);
-        
-        DrawSprite(5, 100, renBroken.Sprite());
 
-        DrawString(6, 221, GetMousePos().str(), olc::BLACK);
-        DrawString(5, 220, GetMousePos().str(), olc::WHITE);
+        // Draw the yellow outline
+        DrawRect(0,0,ScreenWidth()-1, ScreenHeight()-1, olc::YELLOW);
+        
+        // draw some test
+        DrawStringDropShadow(5,  5, "Library Versions Demo", olc::WHITE, olc::BLACK);
+        
+        olc::vi2d header = GetTextSize("LIBRARY             VERSION");
+        olc::vi2d content = GetTextSize(versions);
+
+        DrawRect(2, 45, content.x + 4, header.y + content.y + 7);
+        DrawStringDropShadow(5, 50, "LIBRARY             VERSION", olc::WHITE, olc::BLACK);
+        DrawStringDropShadow(5, 65, versions, olc::WHITE, olc::BLACK);
+
+        DrawStringDropShadow(5, 140, "If a library that can be used\nwith PGEtinker is not listed it\nis because there is no runtime\nmeans to determine it's version.", olc::WHITE, olc::BLACK);
+
+        // draw a circle where the mouse is currently located
         FillCircle(GetMousePos(), 3, olc::RED);
+        
+        // draw a point where the mouse is currently located
         Draw(GetMousePos(), olc::WHITE);
+        
         return true;
+    }
+    
+    void DrawStringDropShadow(const int x, const int y, const std::string& text, const olc::Pixel& foregroundColor, const olc::Pixel& backgroundColor)
+    {
+        DrawString(x + 1, y + 1, text, backgroundColor);
+        DrawString(    x,     y, text, foregroundColor);
     }
     
     olc::Pixel RandomColor()
     {
+        // we limit to the darker half of the colors
         return olc::Pixel(rand() % 128, rand() % 128, rand() % 128);
     }
     
+public: // class variables
+    
     olc::Pixel color;
-    olc::Renderable renBroken;
+    std::string versions;
+    olc::MiniAudio ma;
+    
 };
 
 int main()
 {
+    // an instance of the Example, called demo
     Example demo;
-    if (demo.Construct(256, 240, 2, 2))
+    
+    // attempt to construct the window/screen 256x240 pixels,
+    // with pixels that are 2x2. If successful, start
+    // the demo.
+    if (demo.Construct(320, 180, 4, 4))
         demo.Start();
+    
+    // this is the end of the program
     return 0;
 }
