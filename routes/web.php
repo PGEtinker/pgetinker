4fcef5d12c5f4b1fc5c39f35319ea5d1cea5881a
Moros Smith <moros1138@gmail.com>
2025-04-10 22:49:19 -0400

    main: add /changelog page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Code;
 use Illuminate\Http\Request;
+use Illuminate\Mail\Markdown;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function (Request $request)
@@ -34,4 +35,23 @@ Route::get("/disagree", function()
 Route::get("/player", function ()
 {
     return view("player");
+});
+
+Route::get("/changelog", function()
+{
+    $path = base_path("/CHANGELOG.md");
+    
+    try
+    {
+        if(!file_exists($path))
+            throw new Exception("path not found");
+        
+        $html = Markdown::parse(file_get_contents($path));
+
+        return view("markdown", ["title" => "ChangeLog", "content" => $html]);
+    }
+    catch(Exception $e)
+    {
+        dd($e->getMessage());
+    }
 });
