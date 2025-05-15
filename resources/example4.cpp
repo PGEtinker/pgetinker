b1ffdd4f2d11a2afd9775e07d47d8bc61a8cadc0
Moros Smith <moros1138@gmail.com>
2025-05-15 03:42:41 -0400

    main: flesh out the raylib classic example

--- resources/example4.cpp
+++ resources/example4.cpp
@@ -1,153 +1,177 @@
 /*******************************************************************************************
-*
-*   raylib [shapes] example - Draw basic shapes 2d (rectangle, circle, line...)
-*
-*   Example complexity rating: [★☆☆☆] 1/4
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 4.2
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2014-2025 Ramon Santamaria (@raysan5)
-*
+*   raylib - Classic PGEtinker Example
 ********************************************************************************************/
-
 #include "raylib.h"
 
-#if defined(__EMSCRIPTEN__)
+#include <algorithm>
+#include <iostream>
+#include <stdlib.h>
+
+//------------------------------------------------------------------------------------
+// Allow loading of URL assets on PLATFORM_WEB
+//------------------------------------------------------------------------------------
+#if defined(PLATFORM_WEB)
 #include <emscripten.h>
 #endif
 
-// EMSCRIPTEN ONLY!
-// 
-// At runtime, this function attempts to load a file
-// from the provided URL, and maps it to emscripten's
-// filesystem. You can then use the file in any C/C++
-// filesystem function as if it were on the local disk.
 void FILE_RESOLVE(const char* url, const char* file)
 {
-    #if defined(__EMSCRIPTEN__)
+    #if defined(PLATFORM_WEB)
     emscripten_wget(url, file);
     emscripten_sleep(0);
     #endif
 }
+//------------------------------------------------------------------------------------
 
-#include <iostream> // for cout
-#include <stdlib.h> // for rand
 
-void DrawStringDropShadow(const char* text, int x,  int y, int fontSize, Color foreground, Color background)
+//------------------------------------------------------------------------------------
+// Calculates the rectangles used to scale the canvas
+//------------------------------------------------------------------------------------
+Rectangle CalculateCanvasDestination(float width, float height)
+{
+    float screenWidth = (float)GetScreenWidth();
+    float screenHeight = (float)GetScreenHeight();
+
+    float aspect = width / height;
+    float screenAspect = screenWidth / screenHeight;
+    float scale;
+    
+    Rectangle ret = {0,0,0,0};
+
+    if(aspect > screenAspect)
+    {
+        scale = screenWidth / width;
+        ret.width = screenWidth;
+        ret.height = height * scale;
+        ret.x = 0;
+        ret.y = (screenHeight - ret.height) / 2;
+    }
+    else
+    {
+        scale = screenHeight / height;
+        ret.width = width * scale;
+        ret.height = screenHeight;
+        ret.x = (screenWidth - ret.width) / 2;
+        ret.y = 0;
+    }
+    
+    return ret;
+}
+//------------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------------
+// Get a randomized color of full alpha.
+//------------------------------------------------------------------------------------
+Color GetRandomColor()
+{
+    return Color(rand() % 128, rand() % 128, rand() % 128, 255);
+}
+//------------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------------
+// Draw Text with a drop shadow of the provided foreground/background colors.
+//------------------------------------------------------------------------------------
+void DrawTextDropShadow(const char* text, int x,  int y, int fontSize, Color foreground, Color background)
 {
     DrawText(text, x + 1, y + 1, fontSize, background);
     DrawText(text, x, y, fontSize, foreground);
 }
+//------------------------------------------------------------------------------------
 
-Color GetRandomColor()
-{
-    return Color(rand() % 128, rand() % 128, rand() % 128);
-}
 
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
 int main(void)
 {
+    const int canvasWidth = 256;
+    const int canvasHeight = 240;
+
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-    
-    
-    // raysan5/raylib/master/examples/shaders/resources/shaders/glsl100/pbr.vs
-    // load "assets/gfx/broken.png" from a URL
-    FILE_RESOLVE("https://raw.githubusercontent.com/PGEtinker/assets/main/broken.png", "assets/gfx/broken.png");
-
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(screenWidth, screenHeight, "raylib [shapes] example - basic shapes drawing");
-
+    InitWindow(canvasWidth, canvasHeight, "raylib - PGEtinker Classic Example");
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-    Vector2 mousePos;
-    Color background = GetRandomColor();
+    
+    RenderTexture2D canvas = LoadRenderTexture(canvasWidth, canvasHeight);
+    SetTextureFilter(canvas.texture, TEXTURE_FILTER_POINT);
 
-    Image image = LoadImage("assets/gfx/broken.png");
-    Texture2D texture = LoadTextureFromImage(image);
-    UnloadImage(image);   // Once image has been converted to texture and uploaded to VRAM, it can be unloaded from RAM
+    FILE_RESOLVE("https://raw.githubusercontent.com/PGEtinker/assets/main/broken.png", "assets/gfx/broken.png");
+    Texture2D imageFromURL = LoadTexture("assets/gfx/broken.png");
+    
+    Color background = GetRandomColor();
+    Vector2 realMousePosition = {0, 0};
+    Vector2 mousePosition     = {0, 0};
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        // Core Update
+        //----------------------------------------------------------------------------------
+
+        // Calculate Canvas Position
+        Rectangle canvasSourceRect = { 0, 0, canvasWidth, -canvasHeight };
+        Rectangle canvasDestinationRect = CalculateCanvasDestination(canvasWidth, canvasHeight);
+        
+        // Calculate mouse position 
+        realMousePosition = GetMousePosition();
+        
+        mousePosition.x = realMousePosition.x - canvasDestinationRect.x;
+        mousePosition.y = realMousePosition.y - canvasDestinationRect.y;
+        
+        mousePosition.x = (int)(mousePosition.x / (float)(GetScreenWidth() - (canvasDestinationRect.x * 2)) * canvasWidth);
+        mousePosition.y = (int)(mousePosition.y / (float)(GetScreenHeight() - (canvasDestinationRect.y * 2)) * canvasHeight);
+        
+        mousePosition.x = std::clamp(mousePosition.x, 0.0f, (float)canvasWidth - 1);
+        mousePosition.y = std::clamp(mousePosition.y, 0.0f, (float)canvasHeight - 1);
+        //----------------------------------------------------------------------------------
+
         // Update
         //----------------------------------------------------------------------------------
-        mousePos = GetMousePosition();
 
         if(IsMouseButtonPressed(0))
         {
             background = GetRandomColor();
-            std::cout << TextFormat("(%d,%d)\n", static_cast<int>(mousePos.x), static_cast<int>(mousePos.y));
+            std::cout << TextFormat("(%d,%d)", static_cast<int>(mousePosition.x), static_cast<int>(mousePosition.y)) << "\n";
         }
         //----------------------------------------------------------------------------------
-        
-
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
+            BeginTextureMode(canvas);
+                // clear canvas to color
+                ClearBackground(background);
+                
+                // draw yellow border
+                DrawRectangleLinesEx({0.5f, 0.5f, canvasWidth, canvasHeight - 0.5f}, 1.0f, YELLOW);
+                
+                // draw some text
+                DrawTextDropShadow("Hello, raylib", 5, 5, 8, WHITE, BLACK);
+                DrawTextDropShadow("Mouse position SHOULD match\nclosely to the circle.\n\nYellow borders should ALWAYS\nbe visible\n\nLEFT MOUSE to change color.", 5, 25, 8, WHITE, BLACK);
+                DrawTextDropShadow(TextFormat("(%d,%d)", static_cast<int>(mousePosition.x), static_cast<int>(mousePosition.y)), 5, 220, 8, WHITE, BLACK);
+                
+                // draw image
+                DrawRectangle(5, 115, 16, 16, BLACK);
+                DrawTexture(imageFromURL, 5, 115, WHITE);
 
-            ClearBackground(background);
-            
-            DrawRectangleLines(1, 1, GetScreenWidth()-2, GetScreenHeight()-2, YELLOW);
-            DrawRectangleLines(2, 2, GetScreenWidth()-4, GetScreenHeight()-4, YELLOW);
-            DrawRectangleLines(3, 3, GetScreenWidth()-6, GetScreenHeight()-6, YELLOW);
+                // draw the mouse circles
+                DrawCircle(mousePosition.x+.5f, mousePosition.y, 3.0f, RED);
+                DrawPixel(mousePosition.x+.5f, mousePosition.y+.5f, WHITE);
+            EndTextureMode();
 
-            DrawStringDropShadow(
-                "Hello, raylib",
-                10,
-                10,
-                (GetScreenHeight() / 100) * 4,
-                WHITE,
-                BLACK
-            );
-            
-            DrawStringDropShadow(
-                "Mouse position SHOULD match closely\nto the circle. Yellow borders should\nalways be visible.\n\nLEFT MOUSE to change color.",
-                10,
-                40 + ((GetScreenHeight() / 100) * 4),
-                (GetScreenHeight() / 100) * 4,
-                WHITE,
-                BLACK
-            );
-            
-            DrawStringDropShadow(
-                TextFormat("(%d,%d)", static_cast<int>(mousePos.x), static_cast<int>(mousePos.y)),
-                10,
-                GetScreenHeight() - (20 + (GetScreenHeight() / 100) * 4),
-                (GetScreenHeight() / 100) * 4,
-                WHITE,
-                BLACK
-            );
-            
-            DrawTextureEx(
-                texture,
-                Vector2{
-                    10,
-                    (20 + (static_cast<float>(GetScreenHeight()) / 100) * 4) * 5
-                },
-                0.0f,
-                2.0f,
-                WHITE
-            );
-
-            // Circle shapes and lines
-            DrawCircle(mousePos.x, mousePos.y, (static_cast<float>(GetScreenHeight()) / 100) * 2.0f, RED);
-            DrawCircle(mousePos.x, mousePos.y, (static_cast<float>(GetScreenHeight()) / 100) * 0.3f, WHITE);
-        
+            ClearBackground(BLANK);
+            DrawTexturePro(canvas.texture, canvasSourceRect, canvasDestinationRect, (Vector2){0, 0}, 0.0f, WHITE);
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
+    UnloadTexture(imageFromURL);
+    UnloadRenderTexture(canvas);
     CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
