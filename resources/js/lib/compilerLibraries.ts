f2bfae746ffd728c56fc671fe3fac9150663b44e
Moros Smith <moros1138@gmail.com>
2025-06-07 08:52:36 -0400

    main: filter dev libraries in production

--- resources/js/lib/compilerLibraries.ts
+++ resources/js/lib/compilerLibraries.ts
@@ -1,12 +1,43 @@
 import axios from 'axios';
 import { getStorageValue } from './storage';
 
+interface LibraryVersions {
+  [key: string]: string[];
+}
+
+interface LibrariesManifest {
+  latest: { [key: string]: string };
+  libraries: LibraryVersions;
+  statusCode: number;
+}
+
+interface Window {
+    librariesManifest: LibrariesManifest;
+    AppVersion: string;
+}
+
+declare const window: Window;
+
 export async function getLibrariesManifest(): Promise<void>
 {
-    // @ts-ignore
     await axios.get('/sanctum/csrf-cookie');
-    // @ts-ignore
-    window.librariesManifest = (await axios.get("/api/libraries")).data;
+    const manifest: LibrariesManifest = (await axios.get("/api/libraries")).data;
+    if(!(window.AppVersion.indexOf("develop") !== -1))
+    {
+        window.librariesManifest = {
+            ...manifest,
+            libraries: Object.fromEntries(
+                Object.entries(manifest.libraries).map(([key, versions]) => [
+                    key,
+                    versions.filter(version => version !== "dev")
+                ])
+            )
+        };
+    }
+    else
+    {
+        window.librariesManifest = manifest;
+    }
 }
 
 export function getCompilerLibraries(): any
