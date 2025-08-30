b3b6d0ff1e9bf61397cb59c6666ae92af351977c
Moros Smith <moros1138@gmail.com>
2025-08-30 00:05:56 -0400

    libraries: fix olc_OnPageUnload bug in all versions of PGE

--- shared/libs/olcPixelGameEngine/v2.24/olcPixelGameEngine.h
+++ shared/libs/olcPixelGameEngine/v2.24/olcPixelGameEngine.h
@@ -6188,7 +6188,7 @@ namespace olc {
 
 extern "C" 
 {
-	EMSCRIPTEN_KEEPALIVE inline int olc_OnPageUnload()
+	EMSCRIPTEN_KEEPALIVE inline int olc_OnPageUnload(void* event)
 	{ olc::platform->ApplicationCleanUp(); return 0; }
 }
 
